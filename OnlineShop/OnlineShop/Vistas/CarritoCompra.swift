4db73c6cb7c33707ffe3fa041274eb715729e494
Adrián Rueda <adrian.rueda@ikasle.egibide.org>
2024-03-11 20:07:14 +0100

    actualizacionfirebase

--- OnlineShop/OnlineShop/Vistas/CarritoCompra.swift
+++ OnlineShop/OnlineShop/Vistas/CarritoCompra.swift
@@ -5,16 +5,34 @@
 //  Created by  on 4/3/24.
 //
 
-import Foundation
+
 import SwiftUI
+import FirebaseCore
 
 
-struct CarritoCompra: View {
-    var body: some View {
-        Text("CarritoCompra")
-    }
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
 
+@main
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        CarritoCompra()
+      }
+    }
+  }
+}
 
 #Preview {
     CarritoCompra()
