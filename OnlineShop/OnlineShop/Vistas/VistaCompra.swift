fc43e97e3520a09c10d7de9f5a26642bfa7df456
Jon K. Gomez <jonkandidi.gomez@ikasle.egibide.org>
2024-03-25 20:02:52 +0100

    vistaCompra

--- OnlineShop/OnlineShop/Vistas/VistaCompra.swift
+++ OnlineShop/OnlineShop/Vistas/VistaCompra.swift
@@ -14,19 +14,25 @@ struct VistaCompra: View {
             AsyncImage(url: URL(string:producto.image
                                ))
             { image in
-                image.resizable().scaledToFit()
+                image.resizable()
+                    .scaledToFit()
             } placeholder: {
-            }.padding(.all, 20)
-            Text(producto.title)
-            Text("Descripción")
-            //Rating
-            Button((String(producto.price) + " - Añadir al carrito"), action: {
+            }.padding(.horizontal, 100)
+            Text(producto.title).font(.system(size: 30)).fontWeight(.medium).padding(.vertical, 20)
+            Text("Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday").padding(.vertical, 20)
+            //VistaRating(rating: Int(round(producto.rating.rate)))
+            Button((String(producto.price) + "\u{20AC}" + " - Añadir al carrito"), action: {
                 
-            })
-        }
+            }).padding()
+                .foregroundColor(.white)
+                .background(.purple)
+                .cornerRadius(10)
+                .font(.system(size: 20))
+                .fontWeight(.semibold)
+        }.padding(.horizontal)
     }
 }
 
 #Preview {
-    VistaCompra(producto: Producto(title: "wekhbfqkjbev", price: 123.123, image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"))
+    VistaCompra(producto: Producto(title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", price: 123.123, image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"))
 }
